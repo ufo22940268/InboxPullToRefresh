a23ead3c5bd391f87c036fb46e0be32b62b551dc
ufo22940268 <ufo22940268@gmail.com>
2014-10-31 16:48:48 +0800

    make loading progress act more normal.

--- library/src/main/java/com/bettycc/inboxloading/library/InboxLoading.java
+++ library/src/main/java/com/bettycc/inboxloading/library/InboxLoading.java
@@ -10,6 +10,7 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 
 /**
@@ -17,13 +18,21 @@ import android.view.animation.DecelerateInterpolator;
  */
 public class InboxLoading extends View {
 
-    public static final int SWEEP_RANGE = 100;
+    public static final int SWEEP_RANGE = 270;
+    public static final int SWEEP_DURATION = 600;
+    public static final int ROTATE_DURATION = 2000;
     private float mCircleSize;
     private float mViewSize;
     private float mFastDegree;
     private float mSweep;
-    private float mRotateBaseDegreee;
     private float mRotateDegree;
+    private float mRotateDegreeOffset;
+    private ValueAnimator mSweepDispearAnimator;
+    private ValueAnimator mSweepAppearAnimator;
+
+    private int[] colorRessources = new int[]{
+
+    };
 
     public InboxLoading(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -59,7 +68,9 @@ public class InboxLoading extends View {
                 mCircleSize,
                 mCircleSize);
 
-        canvas.drawArc(rect, mRotateDegree + mRotateBaseDegreee, mSweep, false, paint);
+        int startAngle = (int) (mRotateDegree + mRotateDegreeOffset);
+        System.out.println("startAngle = " + startAngle + "\tsweep = " + mSweep);
+        canvas.drawArc(rect, startAngle, mSweep, false, paint);
     }
 
     private float dipToPx(float dip) {
@@ -71,21 +82,26 @@ public class InboxLoading extends View {
         RotateAnimatorListener rotateAnimatorListener = new RotateAnimatorListener();
         rotateAnimator.addUpdateListener(rotateAnimatorListener);
         rotateAnimator.addListener(rotateAnimatorListener);
-        rotateAnimator.setDuration(3000);
+        rotateAnimator.setDuration(ROTATE_DURATION);
         rotateAnimator.setRepeatMode(ValueAnimator.RESTART);
         rotateAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        rotateAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
         rotateAnimator.start();
 
-        ValueAnimator sweepAnimator = ValueAnimator.ofFloat(0f, SWEEP_RANGE);
-        SweepListener sweepListener = new SweepListener();
-        sweepAnimator.addUpdateListener(sweepListener);
-        sweepAnimator.setInterpolator(new DecelerateInterpolator());
-        sweepAnimator.addListener(sweepListener);
-        sweepAnimator.setDuration(1000);
-        sweepAnimator.setRepeatMode(ValueAnimator.RESTART);
-        sweepAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        sweepAnimator.start();
+        SweepAppearListener sweepAppearListener = new SweepAppearListener();
+        mSweepAppearAnimator = ValueAnimator.ofFloat(0f, SWEEP_RANGE);
+        mSweepAppearAnimator.addUpdateListener(sweepAppearListener);
+        mSweepAppearAnimator.addListener(sweepAppearListener);
+        mSweepAppearAnimator.setInterpolator(new DecelerateInterpolator());
+        mSweepAppearAnimator.setDuration(SWEEP_DURATION);
+
+        SweepDisppearListener sweepDisppearListener = new SweepDisppearListener();
+        mSweepDispearAnimator = ValueAnimator.ofFloat(-SWEEP_RANGE, 0f);
+        mSweepDispearAnimator.addUpdateListener(sweepDisppearListener);
+        mSweepDispearAnimator.addListener(sweepDisppearListener);
+        mSweepDispearAnimator.setInterpolator(new DecelerateInterpolator());
+        mSweepDispearAnimator.setDuration(SWEEP_DURATION);
+
+        mSweepAppearAnimator.start();
     }
 
     private class RotateAnimatorListener implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
@@ -94,6 +110,7 @@ public class InboxLoading extends View {
         public void onAnimationUpdate(ValueAnimator valueAnimator) {
             float v = ((Float) valueAnimator.getAnimatedValue()).floatValue();
             mRotateDegree = v;
+            invalidate();
         }
 
         @Override
@@ -116,16 +133,11 @@ public class InboxLoading extends View {
         }
     }
 
-    private class SweepListener implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
-
-        private boolean reversed = false;
+    private class SweepAppearListener implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
 
         @Override
         public void onAnimationUpdate(ValueAnimator valueAnimator) {
             float v = ((Float) valueAnimator.getAnimatedValue()).floatValue();
-            if (reversed) {
-                v = -(SWEEP_RANGE - v);
-            }
             mSweep = v;
             invalidate();
         }
@@ -137,6 +149,9 @@ public class InboxLoading extends View {
 
         @Override
         public void onAnimationEnd(Animator animator) {
+            mSweepDispearAnimator.start();
+            mRotateDegreeOffset += SWEEP_RANGE;
+            invalidate();
         }
 
         @Override
@@ -146,12 +161,36 @@ public class InboxLoading extends View {
 
         @Override
         public void onAnimationRepeat(Animator animator) {
-            if (!reversed) {
-                System.out.println("SweepListener.onAnimationRepeat");
-                mRotateBaseDegreee += mSweep;
-                mSweep = -SWEEP_RANGE;
-            }
-            reversed = reversed ? false : true;
+        }
+    }
+
+    private class SweepDisppearListener implements Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
+
+        @Override
+        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+            float v = ((Float) valueAnimator.getAnimatedValue()).floatValue();
+            mSweep = v;
+            invalidate();
+        }
+
+        @Override
+        public void onAnimationStart(Animator animator) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animator) {
+            mSweepAppearAnimator.start();
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animator) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animator) {
+
         }
     }
 }
